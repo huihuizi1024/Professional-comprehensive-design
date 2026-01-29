21c3f108716c98a4ea431459f7dc06f0f1ed323a
maken-jhd <2065343055@qq.com>
2026-01-29 21:00:45 +0800

    删掉多余内容，只保留自己工作量

--- server/backend/src/main/java/com/express/cabinet/util/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.express.cabinet.util;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
-
